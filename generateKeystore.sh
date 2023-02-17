465d468e1e5d1ec67701c044567a81d36fed3b55
Tadeo Rivera-Pineda <xtecuan@protonmail.com>
2023-02-17 05:52:08 +0000

    first commit

---
+++ generateKeystore.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export KAPP_HOME=$(dirname $(realpath $0))
+
+source ${KAPP_HOME}/env.sh 
+
+keytool -genkeypair -storepass password -storetype PKCS12 -keyalg RSA -keysize 2048 -dname "${DNAME}" -alias ${ALIAS} -ext "${EXT}" -validity 730  -keystore conf/server.keystore
+
