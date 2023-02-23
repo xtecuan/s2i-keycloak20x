dab0b4c23901b1d08fd0d813c0089e74fcd74f01
Tadeo Rivera-Pineda <xtecuan@protonmail.com>
2023-02-23 06:41:41 +0000

    Otros

--- keycloak20/env.sh
+++ keycloak20/env.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-export CN=qa.apis.authorization.apps.ocp01.promnet.com.sv
-export DNAME="CN=${CN}, OU=IT, O=Banco Promerica, L=Antiguo Cuscatlan, ST=La Libertad, C=SV"
+export CN=keycloak.xtesoft.com
+export DNAME="CN=${CN}, OU=IT, O=Xtesoft INC, L=Santa Ana, ST=Santa Ana, C=SV"
 export ALIAS=server
 export OPENSHIFT_IP=192.168.60.241
-export EXT="SAN=IP:${OPENSHIFT_IP},DNS:${CN},EMAIL:jurivera@promerica.com.sv,EMAIL:gabriel.bran@promerica.com.sv"
+export EXT="SAN=IP:${OPENSHIFT_IP},DNS:${CN},EMAIL:xtecuan@xtesoft.com"
 
 
 
